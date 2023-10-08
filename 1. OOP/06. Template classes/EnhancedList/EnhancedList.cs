e617b4ad46b703bb1422990c7edfd46537ed0a4e
Stefania Tsvetkova <nia_flo@abv.bg>
2023-10-08 13:12:35 +0300

    Added template classes problems

---
+++ 1. OOP/06. Template classes/EnhancedList/EnhancedList.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+
+namespace EnhancedList
+{
+    public class EnhancedList<T> : IEnhancedList<T>
+        where T : IComparable
+    {
+        private List<T> data;
+
+        public EnhancedList()
+        {
+            data = new List<T>();
+        }
+
+        public void Add(T element)
+            => data.Add(element);
+
+        public bool Contains(T element)
+        {
+            foreach (var item in data)
+            {
+                if (item.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int CountGreaterThan(T element)
+        {
+            var count = 0;
+            foreach (var item in data)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public T Max()
+            => GetMinMaxElement((a, b) => a.CompareTo(b) > 0);
+        //{
+        //    if (!data.Any())
+        //    {
+        //        throw new InvalidOperationException("Collection contains no elements");
+        //    }
+
+        //    var maxElement = data[0];
+        //    for (int i = 1; i < data.Count; i++)
+        //    {
+        //        if (data[i].CompareTo(maxElement) > 0)
+        //        {
+        //            maxElement = data[i];
+        //        }
+        //    }
+
+        //    return maxElement;
+        //}
+
+        public T Min()
+            => GetMinMaxElement((a, b) => a.CompareTo(b) < 0);
+        //{
+        //    if (!data.Any())
+        //    {
+        //        throw new InvalidOperationException("Collection contains no elements");
+        //    }
+
+        //    var minElement = data[0];
+        //    for (int i = 1; i < data.Count; i++)
+        //    {
+        //        if (data[i].CompareTo(minElement) < 0)
+        //        {
+        //            minElement = data[i];
+        //        }
+        //    }
+
+        //    return minElement;
+        //}
+
+        public T Remove(int index)
+        {
+            var element = data[index];
+
+            data.RemoveAt(index);
+
+            return element;
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            var buffer = data[index1];
+            data[index1] = data[index2];
+            data[index2] = buffer;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private T GetMinMaxElement(Func<T, T, bool> comparator)
+        {
+            if (!data.Any())
+            {
+                throw new InvalidOperationException("Collection contains no elements");
+            }
+
+            var wantedElement = data[0];
+            for (int i = 1; i < data.Count; i++)
+            {
+                if (comparator(data[i], wantedElement))
+                {
+                    wantedElement = data[i];
+                }
+            }
+
+            return wantedElement;
+        }
+    }
+}
---
+++ 1. OOP/06. Template classes/EnhancedList/Program.cs
@@ -0,0 +1,73 @@
+﻿using EnhancedList;
+
+var list = new EnhancedList<string>();
+
+while (true)
+{
+    var input = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    switch (input[0])
+    {
+        case "Add":
+            HandleAddCommand(input);
+            break;
+        case "Remove":
+            HandleRemoveCommand(input);
+            break;
+        case "Contains":
+            HandleContainsCommand(input);
+            break;
+        case "Swap":
+            HandleSwapCommand(input);
+            break;
+        case "Greater":
+            HandleGreaterCommand(input);
+            break;
+        case "Max":
+            HandleMaxCommand();
+            break;
+        case "Min":
+            HandleMinCommand();
+            break;
+        case "Print":
+            HandlePrintCommand();
+            break;
+        case "END":
+            return;
+    }
+
+}
+
+void HandleAddCommand(string[] input)
+    => list.Add(input[1]);
+
+void HandleRemoveCommand(string[] input)
+{
+    int index = int.Parse(input[1]);
+    list.Remove(index);
+}
+
+void HandleContainsCommand(string[] input)
+    => Console.WriteLine(list.Contains(input[1]));
+
+void HandleSwapCommand(string[] input)
+{
+    int index1 = int.Parse(input[1]);
+    int index2 = int.Parse(input[2]);
+
+    list.Swap(index1, index2);
+}
+
+void HandleGreaterCommand(string[] input)
+    => Console.WriteLine(list.CountGreaterThan(input[1]));
+
+void HandleMaxCommand()
+    => Console.WriteLine(list.Max());
+
+void HandleMinCommand()
+    => Console.WriteLine(list.Min());
+
+void HandlePrintCommand()
+    => Console.WriteLine(list);
