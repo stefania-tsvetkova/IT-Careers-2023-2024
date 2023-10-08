f8fe0fd88b4db6c444b9dc7dc4415c6c5c41bb28
Stefania Tsvetkova <nia_flo@abv.bg>
2023-10-08 13:13:34 +0300

    Added exception problems

---
+++ 1. OOP/08. Exceptions/EnterNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿int ReadNumber(int start, int end)
+{
+    var input = Console.ReadLine();
+
+    int number;
+    try
+    {
+        number = int.Parse(input);
+    }
+    catch (FormatException ex)
+    {
+        throw new FormatException($"{input} is not a number", ex);
+    }
+
+    if (number < start || number > end)
+    {
+        throw new ArgumentOutOfRangeException($"The number should be in the range [{start}, {end}]");
+    }
+
+    return number;
+}
+
+int start = 2, end = 99;
+for (int i = 0; i < 10; i++)
+{
+    try
+    {
+        var currentNumber = ReadNumber(start, end);
+        start = currentNumber + 1;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        i--;
+    }
+}
---
+++ 1. OOP/08. Exceptions/SquareRoot/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = Console.ReadLine();
+
+try
+{
+    var number = int.Parse(input);
+    var squareRoot = Sqrt(number);
+    Console.WriteLine($"Square root of {number} is {squareRoot}");
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("Invalid number");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid number");
+}
+finally
+{
+    Console.WriteLine("Good bye");
+}
+
+double Sqrt(int number)
+{
+    if (number < 0)
+    {
+        throw new ArgumentOutOfRangeException("Number should be non-negative");
+    }
+
+    return Math.Sqrt(number);
+}
