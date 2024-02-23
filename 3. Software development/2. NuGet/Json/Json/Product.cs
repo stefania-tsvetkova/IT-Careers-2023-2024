878d701368e0eca0b115917ac1d258518682ace2
Stefania Tsvetkova <stefania.ts.tsvetkova@gmail.com>
2024-02-24 00:22:47 +0200

    Added NuGet demo

---
+++ 3. Software development/2. NuGet/Json/Json/Product.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Json
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public Product(int id, string name, double price, int stock, DateTime expirationDate)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Stock = stock;
+            ExpirationDate = expirationDate;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Id: {Id}");
+            stringBuilder.AppendLine($"Name: {Name}");
+            stringBuilder.AppendLine($"Price: {Price}");
+            stringBuilder.AppendLine($"Stock: {Stock}");
+            stringBuilder.AppendLine($"ExpirationDate: {ExpirationDate}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ 3. Software development/2. NuGet/Json/Json/Program.cs
@@ -0,0 +1,28 @@
+﻿using Json;
+using Newtonsoft.Json;
+
+var product = new Product(1, "Bread", 2, 50, new DateTime(2024, 2, 20));
+
+var productJson = JsonConvert.SerializeObject(product);
+
+Console.WriteLine(productJson);
+
+var product2 = JsonConvert.DeserializeObject<Product>(productJson);
+
+Console.WriteLine(product2);
+
+Console.WriteLine();
+
+var products = new[]
+{
+    product,
+    new Product(2, "Cheese", 10.50, 20, new DateTime(2024, 3, 15))
+};
+
+var productsJson = JsonConvert.SerializeObject(products);
+
+Console.WriteLine(productsJson);
+
+var products2 = JsonConvert.DeserializeObject<Product[]>(productsJson);
+
+Console.WriteLine(string.Join("\n", products2.Select(p => p.ToString())));
